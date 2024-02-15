e02f4df814aa159f945d4cb27a1e5ae42cf18726
Alberto <151965724+B3RT1C@users.noreply.github.com>
2024-02-15 12:03:07 +0100

    update HorseRacingDuals.java

--- EASY/HorseRacingDuals.java
+++ EASY/HorseRacingDuals.java
@@ -3,7 +3,6 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class HorseRacingDuals {
-
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
